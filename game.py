96a26034141a1821f710aed0ccd7a52fead8930b
Agr3st <patryk.agr3st@gmail.com>
2024-08-07 19:31:57 +0200

    restart possibility added

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 from player import Player
 from board import Board
+from random import randint
 
 
 class Game:
     def __init__(self):
         self.board = Board()
         self.players = [Player("Player 1", "X"), Player("Player 2", "O")]
-        self.current_player = self.players[0]
+        self.current_player = self.players[randint(0, 1)]
 
     def switch_player(self):
         self.current_player = self.players[1] if self.current_player == self.players[0] else self.players[0]
@@ -27,6 +28,16 @@ class Game:
             self.current_player.make_move(self.board)
             self.board.display()
             if self.check_game_over():
-                running = False
+                user_answer = input("Do you want to play again? (Y/N): ")
+                if user_answer.upper() == 'Y':
+                    self.reset()
+                    running = False
+                    self.play()
+                else:
+                    running = False
             else:
                 self.switch_player()
+
+    def reset(self):
+        self.board.grid = [" " for _ in range(9)]
+        self.current_player = self.players[randint(0, 1)]
